12d9b3a53dd4293c14ad698e5b10ccb16ebfbfb2
Stephane Lanjard <stephane.lanjard@kpsport.com>
2023-09-06 18:02:46 +0200

    =Changes of prices

--- src/Synchronization/Product/PricesFromBcSync.php
+++ src/Synchronization/Product/PricesFromBcSync.php
@@ -78,7 +78,7 @@ class PricesFromBcSync
         $this->logger->info('Start with '.$product->getSku());
        
 
-        if(!$product->getPrice()) {
+        if(!$product->getPrice()&& !$product->getResellerPrice()) {
             $this->logger->error('Product has no price');
             return null;
         }
@@ -87,7 +87,7 @@ class PricesFromBcSync
             'sku' => $product->getSku(),
             'specialVat' => $product->getVatCode()=='ESPECIAL' ? 1 : 0,
             'stock' => $this->getFinalStockProductWarehouse($product->getSku(), $product->isBundle()),
-            'priceBB' =>  $product->getPrice(),
+            'priceBB' =>  $product->getPrice() ? $product->getPrice() : $product->getResellerPrice(),
             'pricePublic' => $product->getPublicPrice(),
             'canonDigital' => $product->getCanonDigital()
         ];
