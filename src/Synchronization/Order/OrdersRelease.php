c55cd3c050102982fb27778fc42ca326253d5469
Stephane Lanjard <stephane.lanjard@kpsport.com>
2023-09-22 12:16:03 +0200

    =Add new emails

--- src/Synchronization/Order/OrdersRelease.php
+++ src/Synchronization/Order/OrdersRelease.php
@@ -3,6 +3,7 @@
 namespace App\Synchronization\Order;
 
 use App\BusinessCentral\Connector\KitPersonalizacionSportConnector;
+use App\Entity\Product;
 use App\Entity\SaleOrder;
 use App\Mailer\SendEmail;
 use DateTime;
@@ -38,6 +39,38 @@ class OrdersRelease
                     $saleOrder->addLog('Marked sale order to be released');
                     $saleOrder->setStatus(SaleOrder::STATUS_WAITING_RELEASE);
                     $this->manager->flush();
+                    $minimumPrices = [];
+                    foreach($saleOrder->getSaleOrderLines() as $saleOrderLine) {
+                        if(!array_key_exists($saleOrderLine->getSku(), $minimumPrices)) {
+                            $product = $this->manager->getRepository(Product::class)->findOneBySku($saleOrderLine->getSku());
+                            if($product->getMinimumPrice() && $product->getMinimumPrice() > $saleOrderLine->getPrice()) {
+                                $minimumPrices[$saleOrderLine->getSku()] = [
+                                    'sku'=> $saleOrderLine->getSku(),
+                                    'price'=>$saleOrderLine->getPrice(),
+                                    'minimumPrice'=>$product->getMinimumPrice(),
+                                     "name"=>$product->getNameErp()
+                                    ];
+                            }
+                        }
+                        
+                    }
+
+                    if(count($minimumPrices)>0) {
+                        $content= '<p>The sale order '.$saleOrder->getOrderNumber().' is ready to be released but some prices are below minimum. please request approval to release it</p>';
+                        $content.= '<p>Here the skus and prices that cause problems</p>';
+                        foreach($minimumPrices as $minimumPrice) {
+                            $content.="Product ".$minimumPrice['sku'].' - '.$minimumPrice['name'].' sell '.$minimumPrice['price'].' above minimum price '.$minimumPrice['minimumPrice'].'<br/>';
+                        }
+
+                        $this->sendEmail->sendEmail(
+                            ['administracion@kpsport.com', 'devops@kpsport.com'],
+                             'Need to approval request '.$saleOrder->getOrderNumber(), 
+                             $content
+                        );
+                        $saleOrder->addLog('Sent emails to administracion@kpsport.com to request approval');
+                        $this->manager->flush();
+                    }
+                                       
                 }
             } catch (Exception $e) {
                 $this->logger->critical($e->getMessage().' // '.$e->getFile().' // '.$e->getLine());
