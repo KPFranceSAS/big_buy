ed8a5358d5c36bf18a58bbd11f2117ae293d37fc
Stephane Lanjard <stephane.lanjard@kpsport.com>
2023-10-19 17:12:16 +0200

    =Add sale shipment differnecial

---
+++ migrations/Version20231019121920.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231019121920 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sale_order_line ADD status INT DEFAULT NULL, ADD shipment_number VARCHAR(255) DEFAULT NULL, ADD invoice_number VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sale_order_line DROP status, DROP shipment_number, DROP invoice_number');
+    }
+}
